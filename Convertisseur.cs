f6eb5a1ac6f989710c81f93f45a6bdb27a1c2675
rokaline <caroline.setton@ifosup.be>
2024-02-28 12:07:14 +0100

    ajout des fichiers (code)  pré existants

---
+++ Convertisseur.cs
@@ -0,0 +1,29 @@
+﻿//Convertisseur.cs => pour convertir API
+
+
+//Convertisseur.cs => pour convertir API
+namespace MetropolitanMuseum.Convertisseur
+{
+    using System.Collections.Generic;
+    using System.Text.Json.Serialization;
+
+    // Nouvelle structure pour désérialiser le JSON de la nouvelle API
+    public class DepartmentContainer
+    {
+        [JsonPropertyName("departments")]
+        public List<Department> Departments { get; set; }
+    }
+
+    public class Department
+    {
+        [JsonPropertyName("departmentId")]
+        public int DepartmentId { get; set; }
+
+
+        [JsonPropertyName("displayName")]
+        public string DisplayName { get; set; }
+
+
+        
+    }
+}
---
+++ wwwroot/js/photoDept.js
@@ -0,0 +1,15 @@
+﻿//photoDept.js
+
+//pour agrandissemnt de la photo lors du mousse over
+
+function bigImg(img) {
+    console.log("bigImg called");
+    img.style.width = "200%";
+    img.style.height = "auto";
+}
+
+function normalImg(img) {
+    console.log("normalImg called");
+    img.style.width = "100%";
+    img.style.height = "auto";
+}
