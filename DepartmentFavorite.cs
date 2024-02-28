5ed1b9eb360ef62bbdb32597725f94d90b320b54
rokaline <caroline.setton@ifosup.be>
2024-02-28 23:10:05 +0100

    preparation ajout commentaire & suppression favoris

---
+++ DepartmentFavorite.cs
@@ -0,0 +1,18 @@
+﻿// DepartmentFavorite.cs
+
+using MetropolitanMuseum.Pages;
+using MetropolitanMuseum;
+
+namespace MetropolitanMuseum
+{
+    // données par Classe pour un département
+    public class DepartmentFavorite
+    {
+        public string DepartmentName { get; set; }
+        public string PhotoUrl{ get; set; }
+        public string Description { get; set; }
+        public int Category { get; set; }
+    }
+}
+
+
